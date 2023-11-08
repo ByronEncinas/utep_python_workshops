334a005a77a2a6ad42ed3f54037cb99d05f241ac
byron_encinas <69445567+ByronEncinas@users.noreply.github.com>
2023-11-07 22:00:45 -0700

    added new files

---
+++ CustomLibrary.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def add_evaluate(a,b,c):
+    c = a + b
+
+def substract(a, b):
+    c = -b
+    return a + c
+
+def substract(a, b):
+    c = -b
+    return a + c
+
+def substract_evaluate(num1, num2, result):
+    aux = -num2
+    result = num1 + aux
+
+
+
